dc57b27413dfa9b86e62c8b912f6fc36e1b4636d
Aline Paiva <alinepaivadev@gmail.com>
2025-04-26 21:50:55 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+   const html=document.documentElement
+
+ /*  if(html.classList.contains('Light')) {
+    html.classList.remove('Light') 
+    
+    } else {
+        html.classList.add('Light')
+
+    }
+*/
+html.classList.toggle('light')
+
+}
